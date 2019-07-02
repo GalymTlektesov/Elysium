d085ab552a52cb38459f35ac0278f649dbadb6b5
Galym Tlektesov <galym2000gt@gmail.com>
2019-07-02 13:48:43 +0600

    Добавлен враг, анимация отсутствует, стрелять не умеет

---
+++ Elysium/Assets/Script/AutomatScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomatScript : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float shotDelay;
+    private float nextShot;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        bool canShot = Time.time > nextShot;
+        if (canShot)
+        {
+            Instantiate(bullet, new Vector2(transform.position.x - 1.3f , transform.position.y)
+                , Quaternion.identity);
+            nextShot = Time.time + nextShot;
+        }
+        
+    }
+}
---
+++ Elysium/Assets/Script/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    Rigidbody2D bullet;
+    public float speed = 30;
+    void Start()
+    {
+        Destroy(gameObject, 7);
+    }
+
+    private void Update()
+    {
+        bullet.velocity = Vector2.left * speed;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
