fb99d00828e333e9ebae4f7f11d9c20a65fde4e1
Galym Tlektesov <galym2000gt@gmail.com>
2019-08-05 18:14:38 +0600

    Теперь камера работает плавнее

--- Elysium/Assets/Script/MoveCamera.cs
+++ Elysium/Assets/Script/MoveCamera.cs
@@ -6,6 +6,8 @@ public class MoveCamera : MonoBehaviour
     public Transform player;
     private Transform camera;
 
+    private float pause;
+
     public float x = 3.2f;
     public float y = 3;
 
@@ -13,6 +15,7 @@ public class MoveCamera : MonoBehaviour
     void Awake()
     {
         camera = GetComponent<Transform>();
+        pause = 0.5f;
     }
 
     private void Update()
@@ -27,7 +30,7 @@ public class MoveCamera : MonoBehaviour
             x += 0.2f;
         }
 
-        Invoke("ForUpDate", 0.5f);
+        Invoke("ForUpDate", pause);
     }
 
 
@@ -35,13 +38,14 @@ public class MoveCamera : MonoBehaviour
     {
         if (Math.Abs(player.position.x - camera.position.x) > 3.5f)
         {
-            camera.position = Vector3.MoveTowards(camera.position, new Vector3(player.position.x + x, player.position.y + y, -10), 0.25f);
+            camera.position = Vector3.MoveTowards(camera.position, new Vector3(player.position.x + x, player.position.y + y, -10), 0.2f);
         }
 
         if (Math.Abs(player.position.y - camera.position.y) > 2.5f)
         {
-            camera.position = Vector3.MoveTowards(camera.position, new Vector3(player.position.x + x, player.position.y + y, -10), 0.25f);
+            camera.position = Vector3.MoveTowards(camera.position, new Vector3(player.position.x + x, player.position.y + y, -10), 0.2f);
         }
+        pause = 0;
     }
 
 }
